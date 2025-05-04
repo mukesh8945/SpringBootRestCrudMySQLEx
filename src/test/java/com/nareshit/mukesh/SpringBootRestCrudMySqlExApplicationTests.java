0a35c6fdd16d22696d62a9ed70b9ebd9d64d40f1
mukesh <yryadavmukesh@gmail.com>
2025-05-04 14:05:46 +0530

    initial commit

---
+++ src/test/java/com/nareshit/mukesh/SpringBootRestCrudMySqlExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nareshit.mukesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestCrudMySqlExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
